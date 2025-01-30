b212d7506ffcd211ac5474e6c8ac80b8caaee0bc
Morgan Dean <morgandeanhi@gmail.com>
2025-01-29 23:14:59 -0800

    move to code framework from atspi2-examples, output entire desktop tree to file

--- dump-tree.ts
+++ dump-tree.ts
@@ -1,4 +1,7 @@
 import Atspi from "@girs/atspi-2.0";
+import Gio from "@girs/gio-2.0";
+import GLib from "@girs/glib-2.0";
+import { writeFileSync } from "fs";
 
 function getLabel(accessible: Atspi.Accessible) {
   const relationSet = accessible.get_relation_set();
@@ -12,7 +15,7 @@ function getLabel(accessible: Atspi.Accessible) {
   return null;
 }
 
-function printInfo(accessible: Atspi.Accessible) {
+function formatInfo(accessible: Atspi.Accessible) {
   let name = accessible.get_name();
   if (!name) name = getLabel(accessible);
   const roleName = accessible.get_role_name()!;
@@ -23,9 +26,9 @@ function printInfo(accessible: Atspi.Accessible) {
 function dumpNodeContent(node: Atspi.Accessible, padding: string) {
   let newPadding = padding + "  ";
 
-  const nodeInfo = printInfo(node);
+  const nodeInfo = formatInfo(node);
 
-  print(padding + nodeInfo);
+  out += padding + nodeInfo + "\n";
 
   for (let i = 0; i < node.get_child_count(); i++) {
     const child: Atspi.Accessible | null = node.get_child_at_index(i);
@@ -37,13 +40,62 @@ function dumpNodeContent(node: Atspi.Accessible, padding: string) {
   }
 }
 
-function main() {
+let out = "";
+async function main() {
   Atspi.init();
-  const name = ARGV[0];
 
   const desktop = Atspi.get_desktop(0);
-  for (let i = 0, app; (app = desktop.get_child_at_index(i)); i++)
-    if (app.get_name() === name) dumpNodeContent(app, "  ");
+  for (let i = 0, app; (app = desktop.get_child_at_index(i)); i++) {
+    dumpNodeContent(app, "  ");
+  }
 }
 
-main();
+Gio._promisify(Gio.File.prototype, "copy_async");
+Gio._promisify(Gio.File.prototype, "create_async");
+Gio._promisify(Gio.File.prototype, "delete_async");
+Gio._promisify(Gio.File.prototype, "enumerate_children_async");
+Gio._promisify(Gio.File.prototype, "load_contents_async");
+Gio._promisify(Gio.File.prototype, "make_directory_async");
+Gio._promisify(Gio.File.prototype, "move_async");
+Gio._promisify(Gio.File.prototype, "open_readwrite_async");
+Gio._promisify(Gio.File.prototype, "query_info_async");
+Gio._promisify(Gio.File.prototype, "replace_contents_async");
+Gio._promisify(
+  Gio.File.prototype,
+  "replace_contents_bytes_async",
+  "replace_contents_finish"
+);
+Gio._promisify(Gio.File.prototype, "trash_async");
+
+/* Gio.FileEnumerator */
+Gio._promisify(Gio.FileEnumerator.prototype, "next_files_async");
+
+/* Gio.InputStream */
+Gio._promisify(Gio.InputStream.prototype, "read_bytes_async");
+
+/* Gio.OutputStream */
+Gio._promisify(Gio.OutputStream.prototype, "write_bytes_async");
+
+main().then(async () => {
+  try {
+    const file = Gio.File.new_for_path("out.txt");
+    const enc = new TextEncoder();
+    const bytes = new GLib.Bytes(enc.encode(out));
+
+    // Using the synchronous version for simplicity
+    const [success, etag] = file.replace_contents(
+      //@ts-ignore
+      bytes.get_data(),
+      null, // etag
+      false, // make_backup
+      Gio.FileCreateFlags.REPLACE_DESTINATION,
+      null // cancellable
+    );
+
+    if (success) {
+      print("File written successfully");
+    }
+  } catch (error) {
+    print(`Error writing file: ${(error as Error).message}`);
+  }
+});
